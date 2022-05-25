b8528eb9cdcfe687af3a0f39c48816c2476e5dc3
EC2 Default User <ec2-user@ip-172-31-6-170.ec2.internal>
2022-05-25 15:53:36 +0000

    [Finish] Bookers

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,53 @@
+class BooksController < ApplicationController
+  def show
+    @books = Book.all
+    @book = Book.find(params[:id])
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def create
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
+    redirect_to book_path(@book.id) , notice: 'successfully'
+    else
+    render :index
+    end
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    redirect_to book_path(@book.id), notice: 'successfully'
+    else
+    render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    if book.delete
+    redirect_to books_path, 
+    notice: 'successfully'
+    else
+    render :index
+    end
+  end
+
+  def edit
+    @books = Book.all
+    @book = Book.new
+    @book = Book.find(params[:id])
+  end
+
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
+
